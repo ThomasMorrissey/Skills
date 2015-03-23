c246ec6fc7a0c533dc41acdf0b59b303a8c0c579
LukeGosnell <LGosnell@CIS-06.CIS.COM>
2015-03-23 06:32:39 -0500

    Search and part of Swim

---
+++ Swim.py
@@ -0,0 +1,9 @@
+# Swim skill - Player swims in an area and swim skill affects speed of swim.
+# Waiting for world team to add water to test. (incomplete)
+# Swimming success means swimming 1/2 your normal speed (on land)
+
+class swim_speed(object):
+    def __init__(self):
+        playerspeed = 1
+        self.success = playerspeed/2
+        self.failure = playerspeed*0
